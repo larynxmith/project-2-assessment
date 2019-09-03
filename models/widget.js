3a54191ce60812c793cd70773be53ae26f722e68
larynxmith <larynxmith@gmail.com>
2019-09-03 15:26:58 -0700

    assessment completed

---
+++ models/widget.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const widget = sequelize.define('widget', {
+    description: DataTypes.STRING,
+    quantity: DataTypes.INTEGER
+  }, {});
+  widget.associate = function(models) {
+    // associations can be defined here
+  };
+  return widget;
+};
+
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+db = require('./models')
 
 const app = express();
 
@@ -10,8 +11,39 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+// GET route for Page
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then(widgets => {
+        res.render('index', { widgets })
+    })
+})
+
+//POST route for Widgets
+app.post('/', (req, res) => {
+    db.widget.create(req.body)
+    .then(addedWidget => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('No Go!')
+    })
+})
+
+//DELETE for Widgets
+app.delete('/', (req, res) => {
+    console.log('Delete initiated')
+    db.widget.destroy( {
+        where: req.body
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+})
 
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
 app.listen(3000);
+
